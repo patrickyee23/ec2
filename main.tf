27050428cf11bb856db9d49ede3fd8f8339e728e
Patrick Yee <pyee@23andme.com>
2023-07-19 10:46:27 -0700

    updated with route53

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.8"
     }
   }
 }
 
-variable "type" {
+variable "ami_id" {
   type    = string
   default = "amazon"
 }
@@ -15,11 +15,6 @@ variable "type" {
 variable "instance_type" {
   type    = string
   default = "t3.large"
-}
-
-variable "spot_price" {
-  type    = number
-  default = 0.0832
 }
 
 variable "key_name" {
@@ -32,11 +27,7 @@ provider "aws" {
 }
 
 data "aws_ssm_parameter" "amazon" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2"
-}
-
-data "aws_ssm_parameter" "docker" {
-  name = "/app/latest-ami/docker-runtime-ami/master"
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
 
 data "aws_ssm_parameter" "vpc_id" {
@@ -44,11 +35,15 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 data "aws_ssm_parameter" "ssh_sg_id" {
-  name = "/main/core-sgs/CorpAuthenticatedSourcesSSH/id"
+  name = "/main/core-sgs/CorpVPNSourcesSSH/id"
 }
 
 data "aws_ssm_parameter" "protected_subnet_ids" {
   name = "/main/vpc/subnets/protected/ids"
+}
+
+data "aws_ssm_parameter" "route53_zone_id" {
+  name = "/main/route53/zone/id"
 }
 
 variable "resource_tags" {
@@ -58,30 +53,6 @@ variable "resource_tags" {
     Service            = "coregen"
     Purpose            = "playground"
   }
-}
-
-resource "aws_iam_instance_profile" "profile" {
-  role = aws_iam_role.role.name
-}
-
-resource "aws_iam_role" "role" {
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-               "Service": "ec2.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": "allowassume"
-        }
-    ]
-}
-EOF
 }
 
 resource "aws_security_group" "allow_all_outbound_sg" {
@@ -102,11 +73,9 @@ resource "aws_security_group" "allow_all_outbound_sg" {
 }
 
 resource "aws_spot_instance_request" "my_instance" {
-  ami                  = var.type == "amazon" ? data.aws_ssm_parameter.amazon.value : data.aws_ssm_parameter.docker.value
+  ami                  = var.ami_id == "amazon" ? data.aws_ssm_parameter.amazon.value : var.ami_id
   instance_type        = var.instance_type
   key_name             = var.key_name
-  iam_instance_profile = aws_iam_instance_profile.profile.id
-  spot_price           = var.spot_price
   spot_type            = "one-time"
   subnet_id            = element(split(",", data.aws_ssm_parameter.protected_subnet_ids.value), 1)
   vpc_security_group_ids = [
@@ -118,6 +87,14 @@ resource "aws_spot_instance_request" "my_instance" {
   tags = var.resource_tags
 }
 
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_ssm_parameter.route53_zone_id.value
+  name    = "pyee-ec2"
+  type    = "A"
+  ttl     = 300
+  records = [aws_spot_instance_request.my_instance.private_ip]
+}
+
 output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_spot_instance_request.my_instance.spot_instance_id
@@ -126,4 +103,9 @@ output "instance_id" {
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
   value       = aws_spot_instance_request.my_instance.private_ip
+}
+
+output "instance_route53_fqdn" {
+  description = "FQDN of the EC2 instance"
+  value       = aws_route53_record.record.fqdn
 }
